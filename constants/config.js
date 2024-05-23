27aac6a8ec3ca2bd6e72e1c68087d4eb57c9cab5
harshabonu <152063196+harshabonu@users.noreply.github.com>
2024-05-23 12:56:03 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -4,7 +4,7 @@ const corsOptions = {
     "http://localhost:4173",
     "https://triptalk-liart.vercel.app/",
     "https://triptalk-9gd4hyxk9-harshas-projects-402d176d.vercel.app/",
-    "https://triptalk-y7ks.vercel.app/",
+    "https://triptalk-y7ks.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
